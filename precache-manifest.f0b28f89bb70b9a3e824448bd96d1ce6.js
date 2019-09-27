be9c97f96e8d6846136ea0147d856e40480de1c5
Nathan <n8benzor@gmail.com>
2019-09-27 13:31:26 -0700

    Updates

---
+++ precache-manifest.f0b28f89bb70b9a3e824448bd96d1ce6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a3131ebaafead37f9bfe420d28d56c8",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "26ed69b3befc79fd93f9",
+    "url": "/Clicky-Game/static/css/main.e61e675e.chunk.css"
+  },
+  {
+    "revision": "0475eadd7c5bc04ddf0d",
+    "url": "/Clicky-Game/static/js/2.e9ad7baf.chunk.js"
+  },
+  {
+    "revision": "26ed69b3befc79fd93f9",
+    "url": "/Clicky-Game/static/js/main.02ee8a46.chunk.js"
+  },
+  {
+    "revision": "180153e48e19a7d5d846",
+    "url": "/Clicky-Game/static/js/runtime-main.4a43b3df.js"
+  }
+]);
